a25d4e243fb91e0bbca70985f489e349a72c4ff5
Andrey Mozolev <andrey.mozolev@ostrovok.ru>
2018-11-17 21:48:03 +0300

    Add modules

---
+++ js/pages/intro.js
@@ -0,0 +1,20 @@
+import greeting from './greeting.js';
+import {createElementFromString} from '../utils.js';
+import {renderElement} from '../utils.js';
+
+/** @type {HTMLElement} */
+const intro = createElementFromString(
+    `<section class="intro">
+      <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    </section>`
+);
+
+/** @type {HTMLElement} */
+const asterisk = intro.querySelector(`.intro__asterisk`);
+
+asterisk.addEventListener(`click`, () => {
+  renderElement(greeting);
+});
+
+export default intro;
